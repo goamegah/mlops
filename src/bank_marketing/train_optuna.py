24c093b17762dab2018cead0431da6216c9d0592
goamegah <komlan.godwin.amegah@gmail.com>
2026-06-19 14:18:57 +0200

    fix: add trusted types for XGBoost and LightGBM models in MLflow logging

--- src/bank_marketing/train_optuna.py
+++ src/bank_marketing/train_optuna.py
@@ -352,6 +352,13 @@ def log_family_to_mlflow(
             signature=signature,
             input_example=x_test.iloc[:5],
             registered_model_name=register_as,
+            skops_trusted_types=[
+                "xgboost.sklearn.XGBClassifier",
+                "xgboost.core.Booster",
+                "lightgbm.sklearn.LGBMClassifier",
+                "lightgbm.basic.Booster",
+                "collections.OrderedDict",
+            ],
         )
 
         if register_as and model_info.registered_model_version:
